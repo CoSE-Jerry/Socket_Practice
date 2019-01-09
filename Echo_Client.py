699fad479660b78959a11388c40531e0b1571144
Jerry Miao <jerry@coseinstruments.com>
2019-01-09 10:56:39 -0600

    update

--- Echo_Client.py
+++ Echo_Client.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = '192.168.137.1'  # The server's hostname or IP address
+HOST = '192.168.1.131'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
--- Echo_Server.py
+++ Echo_Server.py
@@ -5,14 +5,27 @@ import socket
 HOST = ''  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+def setupServer():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket created.")
+    try:
+        s.bind((HOST, PORT))
+    except socket.error as msg:
+        print(msg)
+    print("Socket bind comlete.")
+    return s
+
+def setupConnection():
+    s.listen(1) # Allows one connection at a time.
+    conn, address = s.accept()
+    return conn
+
+
+s = setupServer()
+while True:
+    try:
+        conn = setupConnection()
+        #dataTransfer(conn)
+        print("con")
+    except socket.error as msg:
+        print("disconnect")
