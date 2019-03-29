3e38451a56e04b9de291b9d7e9c960e9fc567974
Jerry@Starting_block <jerry@coseinstruments.com>
2019-03-29 15:21:11 -0500

    send addedd

---
+++ Send_Server.py
@@ -0,0 +1,47 @@
+# load additional Python module
+import socket
+
+# create TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# retrieve local hostname
+local_hostname = socket.gethostname()
+
+# get fully qualified hostname
+local_fqdn = socket.getfqdn()
+
+# get the according IP address
+ip_address = "192.168.137.1"
+# output hostname, domain name and IP address
+print ("working on %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
+
+# bind the socket to the port 23456
+server_address = (ip_address, 23456)  
+print ('starting up on %s port %s' % server_address)  
+sock.bind(server_address)
+
+# listen for incoming connections (server mode) with one connection at a time
+sock.listen(1)
+
+while True:  
+    # wait for a connection
+    print ('waiting for a connection')
+    connection, client_address = sock.accept()
+
+    try:
+        # show who connected to us
+        print ('connection from', client_address)
+
+        f = open("recv.jpg",'wb')
+
+        while (True):       
+        # receive data and write it to file
+            l = connection.recv(1024)
+            while (l):
+                f.write(l)
+                l = connection.recv(1024)
+        f.close()
+        
+    finally:
+        # Clean up the connection
+        connection.close()
