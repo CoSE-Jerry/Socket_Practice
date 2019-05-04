804405f5a38d9c6408ebedf9178f0cd4c49dc83d
Jerry@Starting_block <jerry@coseinstruments.com>
2019-05-04 13:29:37 -0500

    Update Echo_Server.py

--- Echo_Server.py
+++ Echo_Server.py
@@ -38,14 +38,18 @@ while True:
         # receive data and write it to file
             l = connection.recv(16384)
             i = 0
+            print ("Receiving Data")
             while (l):
                 i+=1
                 f.write(l)
                 l = connection.recv(16384)
-                print (i)
+            print ("Receiving Done")
+                
                 
         f.close()
         connection.close()
+        print ("connection closed")
+        
         
     finally:
         # Clean up the connection
