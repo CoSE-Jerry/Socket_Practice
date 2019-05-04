7525dd485d044c592c7d1a71a14bd5add30a0784
Jerry@Starting_block <jerry@coseinstruments.com>
2019-05-04 14:39:30 -0500

    Update Echo_Client.py

--- Echo_Client.py
+++ Echo_Client.py
@@ -16,18 +16,11 @@ ip_address = "10.0.5.2"
 # bind the socket to the port 23456, and connect
 server_address = (ip_address, 23456)
 
-def ConvertStringToBytes(src):
-    converted = []
-    for b in src:
-        converted.append(ord(b))
-    return converted
-
 sock.connect(server_address)  
 print ("connecting to %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
 
 while True:
-    data = input("Enter the data to be sent : ")
-    sock.sendall(ConvertStringToBytes(data))
+    sock.sendall('hello')
 
 """f = open ("trans.jpg", "rb")
 l = f.read(1024)
