77c46de9fb3c22e64087879bd439294c73dc4bbe
Jerry@Starting_block <jerry@coseinstruments.com>
2019-05-04 22:30:14 -0500

    Update Echo_Client.py

--- Echo_Client.py
+++ Echo_Client.py
@@ -16,54 +16,49 @@ ip_address = "10.0.5.2"
 # bind the socket to the port 23456, and connect
 server_address = (ip_address, 23456)
 
-sock.connect(server_address)  
-print ("connecting to %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
-
 while True:
-    datas = input("Enter the data to be sent : ")
-    sock.sendall(datas.encode())
-    if(datas == 'A'):
-        '''f = open("recv.jpg",'wb')
+    sock.connect(server_address)  
+    print ("connecting to %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
 
-        l = sock.recv(1024)
-        i=0
-        while (l):       
-        # receive data and write it to file
-            f.write(l)
+    while True:
+        datas = input("Enter the data to be sent : ")
+        sock.sendall(datas.encode())
+        if(datas == 'A'):
+            '''f = open("recv.jpg",'wb')
+
             l = sock.recv(1024)
-            print (i)
-            i+=1
-        print ("Receiving Done")
-        f.close()'''
+            i=0
+            while (l):       
+            # receive data and write it to file
+                f.write(l)
+                l = sock.recv(1024)
+                print (i)
+                i+=1
+            print ("Receiving Done")
+            f.close()'''
+
+            with open('recv.jpg', 'wb') as f:
+                print('file opened')
+                while True:
+                    data = sock.recv(1024)
+                    if not data:
+                        break
+            # write data to a file
+                    f.write(data)
 
 
 
 
-        with open('recv.jpg', 'wb') as f:
-            print('file opened')
-            while True:
-                print('receiving data...')
-                data = sock.recv(1024)
-                if not data:
-                    break
-        # write data to a file
-                f.write(data)
-
-f.close()
 
 
 
 
+    """f = open ("trans.jpg", "rb")
+    l = f.read(1024)
+    while (l):
+        sock.send(l)
+        l = f.read(1024)"""
 
 
-
-
-"""f = open ("trans.jpg", "rb")
-l = f.read(1024)
-while (l):
-    sock.send(l)
-    l = f.read(1024)"""
-
-
-# close connection
-sock.close()  
+    # close connection
+    sock.close()  
